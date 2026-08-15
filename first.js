ff8bc03bb73fdbb55ad51b8d64de762371f5ee54
Aditya Bhardwaj <adibha86045@gmail.com>
2026-08-15 14:56:27 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,17 @@
+console.log("Aditya Bhardwaj ")
+const accountId = 154345
+let accountName = "Aditya Bhardwaj"
+var accountPassword="213123"
+let accountholdercity= "Varanasi"
+let accountHolderState
+
+
+accountName = "Raghunandan"
+accountPassword = "324243"
+accountholdercity = "Pune"
+
+
+console.table({accountId,accountName,accountPassword,accountholdercity,accountHolderState})
+
+console.log(typeof null)
+console.log(typeof undefined)
