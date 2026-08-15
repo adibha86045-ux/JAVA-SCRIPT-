93f6baf55502baab5ab5b822bc1520e15eb5af67
Aditya Bhardwaj <adibha86045@gmail.com>
2026-08-15 15:58:22 +0530

    Add files via upload

---
+++ third.js
@@ -0,0 +1,21 @@
+//OPERATIONS 
+
+let a = 10
+let negativeA = -a
+console.log(negativeA)
+
+//power 
+console.log(a ** 2)
+
+// same data can added or operated with each other
+let str="hello "
+let str2="jagat"
+console.log(str + str2)
+
+//but two different data types cannot be added or operated with each other
+console.log("1" + 2) //this will give 12 as output 
+console.log(1 + "2") //this will give 12 as output 
+console.log(1 + 2) //this will give 3 as output because both are numbers and it will add them
+console.log("1" + "2") //this will give 12 as output
+console.log("1"+2+2) //this will give 122 as output, if first is string then all rest will be treated as string
+console.log(1+2+"2") //this will give 32 as output 
