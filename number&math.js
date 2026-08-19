086fe5ca077544644396a4e2dfdac57213a70f92
Aditya Bhardwaj <adibha86045@gmail.com>
2026-08-19 13:36:06 +0530

    Add files via upload

---
+++ number&math.js
@@ -0,0 +1,34 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance)
+//console.log(balance.toString())
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const othernum = 123.351
+//console.log(othernum.toPrecision(4))
+
+const hundreads = 10000000
+//console.log(hundreads.toLocaleString('en-IN'))
+
+Number.MAX_SAFE_INTEGER
+//console.log(Number.MAX_VALUE)
+
+//------------------MATH------------------
+
+//console.log(Math)
+// console.log(Math.abs(-25));
+// console.log(Math.round(23.4));
+// console.log(Math.ceil(234.2));
+// console.log(Math.floor(23.9));
+// console.log(Math.min(4,54,67,85,34,12));
+// console.log(Math.max(4,54,67,85,34,12));
+
+//console.log(Math.random());//Always between 0 and 1
+//console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
