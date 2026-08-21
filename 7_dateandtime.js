38c8205bdb7ab9b86c6cf2f07c24cd30c5cf5be0
Aditya Bhardwaj <adibha86045@gmail.com>
2026-08-21 17:09:11 +0530

    Add files via upload

---
+++ 7_dateandtime.js
@@ -0,0 +1,33 @@
+// Dates
+let mydate = new Date()
+//console.log(mydate)
+//console.log(mydate.toString())
+//console.log(mydate.toDateString())
+//console.log(mydate.toISOString())
+//console.log(mydate.toJSON())
+//console.log(mydate.toLocaleDateString())
+//console.log(mydate.toLocaleString())
+
+let mycreateddate = new Date(2026,7,21) // MONTHS START FROM 0 NOT 1 
+// console.log (mycreateddate.toDateString())
+
+let mycreateddate2 = new Date(2026,7,21,4,49)
+// console.log (mycreateddate2.toLocaleString())
+
+let mycreatedate3 = new Date("08-21-2026")
+//console.log(mycreatedate3.toLocaleString())
+
+let mytime = Date.now()
+// console.log(mytime)
+// console.log(mycreatedate3.getTime())
+
+//console.log(Math.floor(Date.now()/1000));
+
+let newdatee = new Date()
+// console.log(newdatee)
+// console.log(newdatee.getMonth()+1)
+// console.log(newdatee.getDay())
+
+// newdatee.toLocaleString('default',{
+//     weekday:   "long",
+// })
